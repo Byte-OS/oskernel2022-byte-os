aa4e5ab4a95c2d15529000b0938fc22c700cdc7b
yufeng <321353225@qq.com>
2022-05-15 00:07:04 +0800

    修改fat32信息

--- src/fs/fat32.rs
+++ src/fs/fat32.rs
@@ -31,11 +31,18 @@ pub struct FAT32BPB {
     root_entries: u16,      // 根目录项数 FAT32必须为0
     small_sector: u16,      // 小扇区区数 FAT32必须为0
     media_descriptor: u8,   // 媒体描述符 0xF8标识硬盘 0xF0表示3.5寸软盘
-    sectors_per_fat: u16,   // 每FAT扇区数
+    _sectors_per_fat: u16,  // 每FAT扇区数, 只被FAT12/和FAT16使用 对于FAT32必须设置位0
     sectors_per_track: u16, // 每道扇区数
     number_of_head: u16,    // 磁头数
     hidden_sector: u32,     // 隐藏扇区数
     large_sector: u32,      // 总扇区数
+    sectors_per_fat: u32,   // 每FAT扇区数 只被FAT32使用
+    extended_flag: u16,     // 扩展标志 只被fat32使用
+    filesystem_version: u16,// 文件系统版本
+    root_cluster_numb: u32, // 根目录簇号 只被FAT32使用 根目录第一簇的簇号 一般为2
+    info_sector_numb: u16,  // 文件系统信息扇区号 只被fat32使用
+    backup_boot_sector: u16,// 备份引导扇区
+    reserved_sector1: [u8;12]   // 系统保留
 }
 
 pub enum FAT32FileItemAttr {
@@ -108,19 +115,19 @@ pub struct FAT32longFileItem {
 impl FAT32BPB {
     // 获取数据扇区号
     fn data_sector(&self) -> usize {
-        (self.reserved_sector + self.fat_number as u16 * self.sectors_per_fat) as usize
+        (self.reserved_sector as u32 + self.fat_number as u32 * self.sectors_per_fat) as usize
     }
 
     // 输出fat32信息
     fn info(&self) {
-        info!("变量地址:{:#x}", &(self.jmpcode) as *const _ as usize);
-        info!("磁盘大小:{}", self.large_sector * self.bytes_per_sector as u32);
-        info!("FAT表数量:{}, 占扇区:{}", self.fat_number, self.fat_number as u16 * self.sectors_per_fat);
+        info!("扇区大小: {}", self.bytes_per_sector);
+        info!("磁盘大小:{} bytes", self.large_sector * self.bytes_per_sector as u32);
+        info!("FAT表数量:{}, 占扇区:{}, {:#x}", self.fat_number, self.fat_number as u32 * self.sectors_per_fat, &self.sectors_per_fat as *const u32 as usize - self as *const FAT32BPB as usize);
         info!("保留扇区数: {}, 地址: {:#x}", self.reserved_sector, self.reserved_sector * 512);
         info!("数据扇区: {:#x}", self.data_sector());
         info!("OEM信息:{}", String::from_utf8_lossy(&self.oem));
         info!("根目录数量: {:?}", self.jmpcode);
-        info!("每簇扇区数: {:#x}, {:#x}", self.sectors_per_cluster, (&self.sectors_per_cluster as *const u8 as usize - self as *const FAT32BPB as usize));
+        info!("每簇扇区数: {:#x}", self.sectors_per_cluster);
         info!("隐藏扇区数: {:#x}", self.hidden_sector);
     }
 }
@@ -145,6 +152,8 @@ pub fn init() {
     let mut buf = vec![0u8; 64];
     unsafe {
         fat32.bpb.info();
+
+        info!("数据扇区地址: {:#x}", fat32.bpb.data_sector() << 9);
 
         BLK_CONTROL.read_one_sector(0, fat32.bpb.data_sector(), &mut buf);
         
