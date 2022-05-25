3040b3baf90177598e43fdc0cdabaa8d5375d649
yufeng <321353225@qq.com>
2022-05-26 00:03:47 +0800

    完成sd卡读取

--- src/device/block.rs
+++ src/device/block.rs
@@ -1,24 +1,10 @@
-use core::borrow::Borrow;
-
-use alloc::boxed::Box;
-use alloc::sync::Arc;
-use alloc::vec;
-use alloc::vec::Vec;
 use virtio_drivers::VirtIOBlk;
-use virtio_drivers::VirtIOHeader;
-use crate::fs::fat32::FAT32;
-use crate::sync::mutex::Mutex;
-
 use super::BlockDevice;
 
-const VIRTIO0: usize = 0x10001000;
+pub const VIRTIO0: usize = 0x10001000;
 pub const SECTOR_SIZE: usize = 512;
 
-pub static mut BLK_CONTROL: BlockDeviceContainer = BlockDeviceContainer(vec![]);
-
-pub struct BlockDeviceContainer (Vec<Arc<Mutex<FAT32>>>);
-
-pub struct VirtIOBlock(VirtIOBlk<'static>);
+pub struct VirtIOBlock(pub VirtIOBlk<'static>);
 
 impl BlockDevice for VirtIOBlock {
     fn read_block(&mut self, sector_offset: usize, buf: &mut [u8]) {
@@ -28,32 +14,8 @@ impl BlockDevice for VirtIOBlock {
     fn write_block(&mut self, sector_offset: usize, buf: &mut [u8]) {
         self.0.read_block(sector_offset, buf).expect("写入失败")
     }
-}
 
-impl BlockDeviceContainer {
-    pub fn add(&mut self, virtio: usize) {
-        // 创建存储设备
-        let device = VirtIOBlk::new(unsafe {&mut *(virtio as *mut VirtIOHeader)}).expect("failed to create blk driver");
-        let block_device:Arc<Mutex<Box<dyn BlockDevice>>> = Arc::new(Mutex::new(Box::new(VirtIOBlock(device))));
-        let disk_device = Arc::new(Mutex::new(FAT32::new(block_device)));
-        // device.lock().write_block_nb(block_id, buf, resp)
-        // 识别分区
-        self.0.push(disk_device);
-    }
-
-    // 获取所有文件系统
-    pub fn get_partitions(&self) -> Vec<Arc<Mutex<FAT32>>> {
-        self.0.clone()
-    }
-
-    // 获取分区
-    pub fn get_partition(&self, device_id: usize) -> Arc<Mutex<FAT32>> {
-        self.0[device_id].clone()
-    }
-}
-
-pub fn init() {
-    unsafe {
-        BLK_CONTROL.add(VIRTIO0);
+    fn handle_irq(&mut self) {
+        todo!()
     }
 }
--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,14 +1,71 @@
 pub mod block;
+pub mod sdcard;
 
-pub use block::BLK_CONTROL;
+use alloc::boxed::Box;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
 pub use block::SECTOR_SIZE;
+use virtio_drivers::VirtIOBlk;
+use virtio_drivers::VirtIOHeader;
+
+use crate::fs::fat32::FAT32;
+use crate::sync::mutex::Mutex;
+
+use self::block::VIRTIO0;
+use self::block::VirtIOBlock;
+use self::sdcard::SDCardWrapper;
+
+pub static mut BLK_CONTROL: BlockDeviceContainer = BlockDeviceContainer(vec![]);
 
 pub trait BlockDevice {
     fn read_block(&mut self, sector_offset: usize, buf: &mut [u8]);
 
     fn write_block(&mut self, sector_offset: usize, buf: &mut [u8]);
+
+    fn handle_irq(&mut self);
+}
+
+pub struct BlockDeviceContainer (Vec<Arc<Mutex<FAT32>>>);
+
+
+impl BlockDeviceContainer {
+    pub fn add(&mut self, virtio: usize) {
+        // 创建存储设备
+        let device = VirtIOBlk::new(unsafe {&mut *(virtio as *mut VirtIOHeader)}).expect("failed to create blk driver");
+        let block_device:Arc<Mutex<Box<dyn BlockDevice>>> = Arc::new(Mutex::new(Box::new(VirtIOBlock(device))));
+        let disk_device = Arc::new(Mutex::new(FAT32::new(block_device)));
+        // device.lock().write_block_nb(block_id, buf, resp)
+        // 识别分区
+        self.0.push(disk_device);
+    }
+
+    pub fn add_sdcard(&mut self) {
+        // 创建存储设备
+        let block_device:Arc<Mutex<Box<dyn BlockDevice>>> = Arc::new(Mutex::new(Box::new(SDCardWrapper::new())));
+        let disk_device = Arc::new(Mutex::new(FAT32::new(block_device)));
+        // device.lock().write_block_nb(block_id, buf, resp)
+        // 识别分区
+        self.0.push(disk_device);
+    }
+
+    // 获取所有文件系统
+    pub fn get_partitions(&self) -> Vec<Arc<Mutex<FAT32>>> {
+        self.0.clone()
+    }
+
+    // 获取分区
+    pub fn get_partition(&self, device_id: usize) -> Arc<Mutex<FAT32>> {
+        self.0[device_id].clone()
+    }
 }
 
 pub fn init() {
-    block::init();
+    #[cfg(not(feature = "board_k210"))]
+    unsafe {
+        BLK_CONTROL.add(VIRTIO0);
+    }
+    #[cfg(feature = "board_k210")]
+    unsafe {
+        BLK_CONTROL.add_sdcard();
+    }
 }
