0dd0b0c32a4796cb9deff7c24d7216cc3bfde745
yufeng <321353225@qq.com>
2022-08-14 18:58:56 +0800

    更新task queue

--- kernel/src/task/task_queue.rs
+++ kernel/src/task/task_queue.rs
@@ -8,16 +8,16 @@ use super::exec;
 
 lazy_static! {
     pub static ref TASK_QUEUE: Mutex<VecDeque<&'static str>> = Mutex::new(VecDeque::from(vec![
-        "busybox sh busybox_testcode.sh",
-        "busybox sh test.sh date.lua",
-        "busybox sh test.sh file_io.lua",
-        "busybox sh test.sh max_min.lua",
-        "busybox sh test.sh random.lua",
-        "busybox sh test.sh remove.lua",
-        "busybox sh test.sh round_num.lua",
-        "busybox sh test.sh sin30.lua",
-        "busybox sh test.sh sort.lua",
-        "busybox sh test.sh strings.lua",
+        // "busybox sh busybox_testcode.sh",
+        // "busybox sh test.sh date.lua",
+        // "busybox sh test.sh file_io.lua",
+        // "busybox sh test.sh max_min.lua",
+        // "busybox sh test.sh random.lua",
+        // "busybox sh test.sh remove.lua",
+        // "busybox sh test.sh round_num.lua",
+        // "busybox sh test.sh sin30.lua",
+        // "busybox sh test.sh sort.lua",
+        // "busybox sh test.sh strings.lua",
         
         // sleep
 
@@ -27,6 +27,43 @@ lazy_static! {
         "lmbench_all lat_syscall -P 1 null",
         "lmbench_all lat_syscall -P 1 read",
         "lmbench_all lat_syscall -P 1 write",
+        "sleep",
+        "busybox mkdir -p /var/tmp",
+        "busybox touch /var/tmp/lmbench",
+        "sleep",
+        "lmbench_all lat_syscall -P 1 stat /var/tmp/lmbench",
+        "lmbench_all lat_syscall -P 1 fstat /var/tmp/lmbench",
+        "lmbench_all lat_syscall -P 1 open /var/tmp/lmbench",
+        "lmbench_all lat_select -n 100 -P 1 file",
+        // "lmbench_all lat_sig -P 1 install",
+        // "lmbench_all lat_sig -P 1 catch",
+        // "lmbench_all lat_sig -P 1 prot lat_sig",
+        // "lmbench_all lat_pipe -P 1",
+        // "lmbench_all lat_proc -P 1 fork",
+        // "lmbench_all lat_proc -P 1 exec",
+        "sleep",
+        "busybox cp hello /tmp",
+        "sleep",
+        // "lmbench_all lat_proc -P 1 shell",
+        "lmbench_all lmdd label=\"File /var/tmp/XXX write bandwidth:\" of=/var/tmp/XXX move=645m fsync=1 print=3",
+        "lmbench_all lat_pagefault -P 1 /var/tmp/XXX",
+        "lmbench_all lat_mmap -P 1 512k /var/tmp/XXX",
+        "sleep",
+        "busybox echo file system latency",
+        "sleep",
+        // "lmbench_all lat_fs /var/tmp",
+        "sleep",
+        "busybox echo Bandwidth measurements",
+        "sleep",
+        // "lmbench_all bw_pipe -P 1",
+        "lmbench_all bw_file_rd -P 1 512k io_only /var/tmp/XXX",
+        "lmbench_all bw_file_rd -P 1 512k open2close /var/tmp/XXX",
+        "lmbench_all bw_mmap_rd -P 1 512k mmap_only /var/tmp/XXX",
+        "lmbench_all bw_mmap_rd -P 1 512k open2close /var/tmp/XXX",
+        "sleep",
+        "busybox echo context switch overhead",
+        "sleep",
+        "lmbench_all lat_ctx -P 1 -s 32 2 4 8 16 24 32 64 96",
 
     ]));
 }
@@ -46,6 +83,7 @@ pub fn load_next_task() -> bool {
     loop {
         if let Some(pro_name) = TASK_QUEUE.lock().pop_front() {
             if pro_name == "sleep" {
+                #[cfg(feature = "board_k210")]
                 usleep(100000);
                 continue;
             }
