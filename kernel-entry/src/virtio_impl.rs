06978800cf262129fe90f7fd5a72338d13a915e6
yufeng <321353225@qq.com>
2022-09-01 21:09:44 +0800

    更新文档注释

--- kernel-entry/src/virtio_impl.rs
+++ kernel-entry/src/virtio_impl.rs
@@ -1,32 +1,36 @@
 
-use crate::memory::{page::PAGE_ALLOCATOR, addr::{PhysPageNum, PhysAddr, VirtAddr}};
+use crate::memory::addr::{PhysPageNum, PhysAddr, VirtAddr};
+use crate::memory::page::PAGE_ALLOCATOR;
 
-
+/// dma内存申请
+/// 
+/// 申请内存作为virtio的dma内存
 #[no_mangle]
 extern "C" fn virtio_dma_alloc(pages: usize) -> PhysAddr {
-    info!("申请设备地址!");
-    if let Ok(page_num) = PAGE_ALLOCATOR.lock().alloc_more(pages) {
-        let addr = PhysAddr::from(page_num);
-        info!("alloc DMA: {:?}, pages={}", addr, pages);
-        return addr
-    } else {
-        panic!("申请失败");
-    }
-    // let paddr = DMA_PADDR.fetch_add(0x1000 * pages, Ordering::SeqCst);
+    PAGE_ALLOCATOR.lock().alloc_more(pages).expect("virtio dma内存申请失败").into()
 }
 
+/// dma内存释放
+/// 
+/// 释放virtio 使用的dma内存
 #[no_mangle]
 extern "C" fn virtio_dma_dealloc(paddr: PhysAddr, pages: usize) -> i32 {
     PAGE_ALLOCATOR.lock().dealloc_more(PhysPageNum::from(paddr), pages);
     0
 }
 
+/// 物理内存转为虚拟内存
+/// 
+/// 因为在内核中恒等映射，所以直接转换直接返回即可
 #[no_mangle]
 extern "C" fn virtio_phys_to_virt(paddr: PhysAddr) -> VirtAddr {
-    VirtAddr::from(usize::from(paddr))
+    paddr.0.into()
 }
 
+/// 虚拟内存转为物理内存
+/// 
+/// 因为在内核中恒等映射，所以直接返回即可
 #[no_mangle]
 extern "C" fn virtio_virt_to_phys(vaddr: VirtAddr) -> PhysAddr {
-    PhysAddr::from(usize::from(vaddr))
+    vaddr.0.into()
 }
