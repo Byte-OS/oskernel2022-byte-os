20850fc57b8cfb6da9b79148d160ee71a4c7d005
yufeng <321353225@qq.com>
2022-09-01 20:39:03 +0800

    更新入口函数

--- kernel-entry/src/main.rs
+++ kernel-entry/src/main.rs
@@ -15,10 +15,7 @@ extern crate alloc;
 mod virtio_impl;
 
 use alloc::rc::Rc;
-use alloc::string::ToString;
 use kernel::fs::cache::cache_file;
-use kernel::fs::file::FileType;
-use kernel::fs::filetree::DiskFileEnum;
 use kernel::fs::filetree::INode;
 use kernel::memory;
 use kernel::interrupt;
@@ -28,13 +25,14 @@ use riscv::register::sstatus;
 use task_scheduler::start_tasks;
 use arch::sbi;
 
-// global_asm!(include_str!("entry.asm"));
-
+/// 汇编入口函数
+/// 
+/// 分配栈 并调到rust入口函数
 #[naked]
 #[no_mangle]
 #[link_section = ".text.entry"]
 unsafe extern "C" fn _start() -> ! {
-    const STACK_SIZE: usize = 4096 * 16;
+    const STACK_SIZE: usize = 4096;
 
     #[link_section = ".bss.stack"]
     static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
@@ -49,6 +47,7 @@ unsafe extern "C" fn _start() -> ! {
     )
 }
 
+/// rust 入口函数
 #[no_mangle]
 pub extern "C" fn rust_main(hart_id: usize, device_tree_p_addr: usize) -> ! {
     // // 保证仅有一个核心工作
@@ -59,6 +58,12 @@ pub extern "C" fn rust_main(hart_id: usize, device_tree_p_addr: usize) -> ! {
 
     unsafe {
         sstatus::set_fs(sstatus::FS::Dirty);
+
+        // 开启SUM位 让内核可以访问用户空间  踩坑：  
+        // only in qemu. eg: qemu is riscv 1.10    k210 is riscv 1.9.1  
+        // in 1.10 is SUM but in 1.9.1 is PUM which is the opposite meaning with SUM
+        #[cfg(not(feature = "board_k210"))]
+        sstatus::set_sum();
     }
 
     // 输出设备信息
@@ -67,14 +72,6 @@ pub extern "C" fn rust_main(hart_id: usize, device_tree_p_addr: usize) -> ! {
 
     // 提示信息
     info!("Welcome to Byte os!");
-
-    // 开启SUM位 让内核可以访问用户空间  踩坑：  
-    // only in qemu. eg: qemu is riscv 1.10    k210 is riscv 1.9.1  
-    // in 1.10 is SUM but in 1.9.1 is PUM which is the opposite meaning with SUM
-    #[cfg(not(feature = "board_k210"))]
-    unsafe {
-        sstatus::set_sum();
-    }
 
     // 初始化内存
     memory::init();
@@ -99,49 +96,33 @@ pub extern "C" fn rust_main(hart_id: usize, device_tree_p_addr: usize) -> ! {
     busybox_node.linkat("cp");
     busybox_node.linkat("ls");
     busybox_node.linkat("pwd");
-    INode::mkdir(None, "/bin", 0).expect("can't create bin directory");
-    INode::mkdir(None, "/sbin", 0).expect("can't create sbin directory");
-    busybox_node.linkat("bin/busybox");
-    let lmbench_all = INode::get(None, "lmbench_all").expect("can't find busybox");
-    lmbench_all.linkat("sbin/lmbench_all");
-    lmbench_all.linkat("bin/lmbench_all");
-    // let lmbench_all = INode::get(None, "busybox_cmd.txt").expect("can't find busybox");
-    lmbench_all.linkat("var/tmp/XXX");
 
-    INode::root().add(INode::new("proc".to_string(), 
-        DiskFileEnum::None, FileType::Directory, None));
+    // 缓冲文件 加快系统处理
+    cache_file("busybox");
+    cache_file("lua");
 
-    #[cfg(not(feature = "board_k210"))]
-    {
-        // 非k210缓冲文件
-        cache_file("busybox");
-        cache_file("lua");
-        cache_file("lmbench_all");
-    }
-
-    // // 初始化多任务
-    // task::init();
+    // 初始化多任务
     start_tasks();
 
     // 调用rust api关机
     panic!("正常关机")
 }
 
+/// 辅助核心恢复 暂时不使用  目前只使用单核
 #[allow(unused)]
-/// 暂时不使用  目前只使用单核
 extern "C" fn support_hart_resume(hart_id: usize, _param: usize) {
     info!("核心 {} 作为辅助核心进行等待", hart_id);
     loop {} // 进入循环
 }
 
 
-// 打印目录树
+/// 打印目录树
 pub fn print_file_tree(node: Rc<INode>) {
     info!("{}", node.get_pwd());
     print_file_tree_back(node, 0);
 }
 
-// 打印目录树 - 递归
+/// 打印目录树 - 递归
 pub fn print_file_tree_back(node: Rc<INode>, space: usize) {
     let iter = node.clone_children();
     let mut iter = iter.iter().peekable();
